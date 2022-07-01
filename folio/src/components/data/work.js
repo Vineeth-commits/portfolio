7f64d15327aa884a4f8a504540b165d807fe462b
Vineeth-commits <vineethnreddy@gmail.com>
2022-07-01 23:39:28 +0530

    Added another internship

--- folio/src/components/data/work.js
+++ folio/src/components/data/work.js
@@ -7,5 +7,12 @@ export const WorkData=[
         companyLogo:require("../../assets/Koç_University_logo.svg").default,
         work: "Helped developing Gaussian packages under prof. Carlson",
     },
-    
+   {
+      company:"Mulltiply",
+      designation:"Data Analyst",
+      dateJoining:"1 March 2022",
+      dateEnd:"1 June 2022",
+      companyLogo:require("../../assets/mulltiply.svg").default,
+      work:"Designing and maintaining data systems and databases; this  includes  fixing coding errors and other data-related problems",
+   },
 ];
